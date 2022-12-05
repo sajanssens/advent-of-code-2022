a1394fc62fa8d727a644addfb6167b72c4fb2d52
Bram Janssens <s.a.janssens@gmail.com>
2022-12-05 11:30:10 +0100

    Day3 part 1

---
+++ src/main/kotlin/nl/bramjanssens/Day03.kt
@@ -0,0 +1,22 @@
+package nl.bramjanssens
+
+import nl.bramjanssens.InputType.Main
+
+fun main() {
+    // part 1
+    println(
+        lines(3, Main)
+            .map { chop(it) }
+            .map { intersect(it) }
+            .sumOf { prio(it) }
+    )
+
+    // part 2
+    lines(3, Main)
+}
+
+private fun chop(s: String) = s.substring(0, s.length / 2) to s.substring(s.length / 2)
+
+private fun intersect(p: Pair<String, String>) = p.first.toSet().intersect(p.second.toSet()).first()
+
+private fun prio(f: Char) = if (f in 'a'..'z') f.code - 96 else f.code - 38
