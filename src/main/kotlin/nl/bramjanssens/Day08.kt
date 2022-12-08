c6dad2eabb5b620ab3a31518f75ac8d50580872a
Bram Janssens <s.a.janssens@gmail.com>
2022-12-08 11:24:25 +0100

    Day8 part 2.

--- src/main/kotlin/nl/bramjanssens/Day08.kt
+++ src/main/kotlin/nl/bramjanssens/Day08.kt
@@ -1,59 +1,95 @@
 package nl.bramjanssens
 
+import nl.bramjanssens.InputType.Main
+
 fun main() {
-    val lines = lines(8, InputType.Main)
-    val grid = Grid(lines)
-    println(grid.countVisibleTrees())
+    val grid = Grid(lines(8, Main))
+    println("part 1: ${grid.countVisibleTrees()}")
+    println("part 2: ${grid.scenicScores().max()}")
+
 }
 
 class Grid(lines: List<String>) {
     private val width = lines.size
-    private val cells = Array(width) { IntArray(width) }
+    private val trees = Array(width) { IntArray(width) }
 
     init {
         lines.withIndex().forEach { addRow(it.index, it.value) }
     }
 
     private fun addRow(i: Int, row: String) {
-        cells[i] = row.split("").filter { it.isNotBlank() }.map { it.toInt() }.toIntArray()
+        trees[i] = row.split("").filter { it.isNotBlank() }.map { it.toInt() }.toIntArray()
     }
 
-    private fun allNorthOf(r: Int, c: Int) =
+    private fun northOf(r: Int, c: Int) =
         mutableListOf<Int>().apply {
-            for (i in r - 1 downTo 0) add(cells[i][c])
-        }
-
-    private fun allSouthOf(r: Int, c: Int) =
-        mutableListOf<Int>().apply {
-            for (i in r + 1 until width) add(cells[i][c])
+            for (i in r - 1 downTo 0) add(trees[i][c])
         }.toList()
 
-    private fun allWestOf(r: Int, c: Int) =
+    private fun southOf(r: Int, c: Int) =
         mutableListOf<Int>().apply {
-            for (i in c - 1 downTo 0) add(cells[r][i])
+            for (i in r + 1 until width) add(trees[i][c])
         }.toList()
 
-    private fun allEastOf(r: Int, c: Int) =
+    private fun westOf(r: Int, c: Int) =
         mutableListOf<Int>().apply {
-            for (i in c + 1 until width) add(cells[r][i])
+            for (i in c - 1 downTo 0) add(trees[r][i])
+        }.toList()
+
+    private fun eastOf(r: Int, c: Int) =
+        mutableListOf<Int>().apply {
+            for (i in c + 1 until width) add(trees[r][i])
         }.toList()
 
     fun countVisibleTrees(): Int {
-        var high = (width - 1) * 4
+        var count = (width - 1) * 4
 
         for (row in 1 until width - 1) {
             for (col in 1 until width - 1) {
-                val c = cells[row][col]
+                val c = trees[row][col]
 
-                val north = allNorthOf(row, col)
-                val east = allEastOf(row, col)
-                val south = allSouthOf(row, col)
-                val west = allWestOf(row, col)
-
-                if (north.all { it < c } || east.all { it < c } || south.all { it < c } || west.all { it < c }) high++
+                if (northOf(row, col).all { it < c } ||
+                    eastOf(row, col).all { it < c } ||
+                    southOf(row, col).all { it < c } ||
+                    westOf(row, col).all { it < c }) {
+                    count++
+                }
             }
         }
-        return high
+
+        return count
     }
 
+    fun scenicScores(): List<Int> {
+        val scores = mutableListOf<Int>()
+
+        for (row in 1 until width - 1) {
+            for (col in 1 until width - 1) {
+                scores.add(scenicScoreOf(row, col))
+            }
+        }
+        return scores
+    }
+
+    private fun scenicScoreOf(row: Int, col: Int): Int {
+        val tree = trees[row][col]
+
+        return score(northOf(row, col), tree) *
+                score(eastOf(row, col), tree) *
+                score(southOf(row, col), tree) *
+                score(westOf(row, col), tree)
+    }
+
+    private fun score(trees: List<Int>, it: Int): Int {
+        var count = 0
+        for (tree in trees) {
+            if (tree >= it) {
+                count++;
+                break
+            } else {
+                count++
+            }
+        }
+        return count
+    }
 }
