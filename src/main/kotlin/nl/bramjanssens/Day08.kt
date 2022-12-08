23a9ddbca3e9e1c662e3150f67e1664f135df144
Bram Janssens <s.a.janssens@gmail.com>
2022-12-08 09:56:07 +0100

    Day8 part 1 refactored.

--- src/main/kotlin/nl/bramjanssens/Day08.kt
+++ src/main/kotlin/nl/bramjanssens/Day08.kt
@@ -2,49 +2,47 @@ package nl.bramjanssens
 
 fun main() {
     val lines = lines(8, InputType.Main)
-    val field = Field(lines.size)
-    for ((i, row) in lines.withIndex()) {
-        field.addRow(i, row)
-    }
-    println(field.countHighTrees())
+    val grid = Grid(lines)
+    println(grid.countVisibleTrees())
 }
 
-data class Field(val size: Int) {
-    private val cells = Array(size) { IntArray(size) }
+class Grid(lines: List<String>) {
+    private val width = lines.size
+    private val cells = Array(width) { IntArray(width) }
 
-    fun addRow(i: Int, row: String) {
+    init {
+        lines.withIndex().forEach { addRow(it.index, it.value) }
+    }
+
+    private fun addRow(i: Int, row: String) {
         cells[i] = row.split("").filter { it.isNotBlank() }.map { it.toInt() }.toIntArray()
     }
 
-    private fun allNorthOf(r: Int, c: Int): List<Int> {
-        val cells = mutableListOf<Int>()
-        for (i in r - 1 downTo 0) cells.add(this.cells[i][c])
-        return cells.toList()
-    }
+    private fun allNorthOf(r: Int, c: Int) =
+        mutableListOf<Int>().apply {
+            for (i in r - 1 downTo 0) add(cells[i][c])
+        }
 
-    private fun allSouthOf(r: Int, c: Int): List<Int> {
-        val cells = mutableListOf<Int>()
-        for (i in r + 1 until size) cells.add(this.cells[i][c])
-        return cells.toList()
-    }
+    private fun allSouthOf(r: Int, c: Int) =
+        mutableListOf<Int>().apply {
+            for (i in r + 1 until width) add(cells[i][c])
+        }.toList()
 
-    private fun allWestOf(r: Int, c: Int): List<Int> {
-        val cells = mutableListOf<Int>()
-        for (i in c - 1 downTo 0) cells.add(this.cells[r][i])
-        return cells.toList()
-    }
+    private fun allWestOf(r: Int, c: Int) =
+        mutableListOf<Int>().apply {
+            for (i in c - 1 downTo 0) add(cells[r][i])
+        }.toList()
 
-    private fun allEastOf(r: Int, c: Int): List<Int> {
-        val cells = mutableListOf<Int>()
-        for (i in c + 1 until size) cells.add(this.cells[r][i])
-        return cells.toList()
-    }
+    private fun allEastOf(r: Int, c: Int) =
+        mutableListOf<Int>().apply {
+            for (i in c + 1 until width) add(cells[r][i])
+        }.toList()
 
-    fun countHighTrees(): Int {
-        var high = (size - 1) * 4
+    fun countVisibleTrees(): Int {
+        var high = (width - 1) * 4
 
-        for (row in 1 until size - 1) {
-            for (col in 1 until size - 1) {
+        for (row in 1 until width - 1) {
+            for (col in 1 until width - 1) {
                 val c = cells[row][col]
 
                 val north = allNorthOf(row, col)
