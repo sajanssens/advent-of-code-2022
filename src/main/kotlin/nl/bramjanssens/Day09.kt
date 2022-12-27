6dfb590ab8439e3d339d37c1162a39561f934b9b
Bram Janssens <s.a.janssens@gmail.com>
2022-12-27 16:56:12 +0100

    Day9 part 2 attempt.

--- src/main/kotlin/nl/bramjanssens/Day09.kt
+++ src/main/kotlin/nl/bramjanssens/Day09.kt
@@ -1,9 +1,15 @@
 package nl.bramjanssens
 
 import nl.bramjanssens.InputType.Main
+import nl.bramjanssens.InputType.Test
 import kotlin.math.absoluteValue
 
 fun main() {
+    // part1()
+    part2()
+}
+
+private fun part1() {
     val head = Knot(0, 0)
     val tail = Knot(0, 0)
     val tailMoves = mutableSetOf<Point>()
@@ -14,6 +20,30 @@ fun main() {
             head.move(dir)
             val tailMove = tail.move(dir, head)
             tailMoves.add(tailMove)
+        }
+    }
+
+    println(tailMoves.count())
+}
+
+private fun part2() {
+    val knots = List(10) { Knot(0, 0) }
+    val head = knots.first()
+    val tail = knots.last()
+    val tailMoves = mutableSetOf<Point>()
+
+    for (line in lines(9, Test)) {
+        val (dir, steps) = line.split(" ")
+
+        repeat(steps.toInt()) {
+            head.move(dir)
+
+            for ((k1, k2) in knots.zipWithNext()) {
+                val move = k2.move(dir, k1)
+                if (k2 === tail) {
+                    tailMoves.add(move)
+                }
+            }
         }
     }
 
@@ -42,7 +72,7 @@ data class Knot(var x: Int, var y: Int) {
 
     private fun adjacentTo(other: Knot) = (y - other.y).absoluteValue <= 1 && (x - other.x).absoluteValue <= 1
 
-    private fun diagonalTo(other: Knot) = (y - other.y).absoluteValue == 1 && (x - other.x).absoluteValue == 1
+    private fun diagonalTo(other: Knot) = (y - other.y).absoluteValue >= 1 && (x - other.x).absoluteValue >= 1
 
     private fun correctDiagonal(dir: String, head: Knot) {
         if (dir.horizontal) y = head.y
